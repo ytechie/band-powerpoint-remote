3ae5a6e0d93ed819f5c050fd6f04fa7c013178dd
Jason Young <jason@ytechie.com>
2015-06-10 10:50:32 -0700

    Skeleton Project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # band-powerpoint-remote
+
+Still a work in progress. Trying to get the band to control PowerPoint using gestures.
---
+++ WinPhone/MainPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Band;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace WinPhone
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+            StartBandMonitor();
+        }
+
+        private async void StartBandMonitor()
+        {
+            // Get the list of Microsoft Bands paired to the phone.
+            var pairedBands = await BandClientManager.Instance.GetBandsAsync();
+            if (pairedBands.Length < 1)
+            {
+                throw new Exception("Can't find a band");
+            }
+
+            // Connect to Microsoft Band.
+            using (var bandClient = await BandClientManager.Instance.ConnectAsync(pairedBands[0]))
+            {
+                int samplesReceived = 0; // the number of Accelerometer samples received
+
+                // Subscribe to Accelerometer data.
+                bandClient.SensorManager.Accelerometer.ReadingChanged += (s, args) => { samplesReceived++; };
+                await bandClient.SensorManager.Accelerometer.StartReadingsAsync();
+            }
+        }
+    }
+}
